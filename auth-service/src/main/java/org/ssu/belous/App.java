ab291a601869cd1636ba1f234122e88e5476fbdd
Alexei Belous <dienels_izzi@mail.ru>
2025-03-24 11:36:29 +0400

    junit в общий помник

---
+++ auth-service/src/main/java/org/ssu/belous/App.java
@@ -0,0 +1,13 @@
+package org.ssu.belous;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
