1ac260986ba358808c2d86e3cdc311bc236c4e88
Alexei Belous <dienels_izzi@mail.ru>
2025-03-24 10:40:34 +0400

    Завел рыбу проекта

---
+++ delivery-service/src/main/java/org/ifellow/belous/App.java
@@ -0,0 +1,13 @@
+package org.ifellow.belous;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
